e2e9242ae60d64e875c420171530f8e8251092cb
andrea <corro.andrea.1995@gmail.com>
2021-04-13 16:16:46 +0200

    Plate json, ricerca piatti

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,32 @@
 require('./bootstrap');
+import Vue from 'vue';
+
+var chiamate = new Vue({
+  el: '#app',
+  data: {
+    array: '',
+    search: '',
+    piattiSelezionati:[]
+  },
+  mounted(){
+    axios
+    .get('http://localhost:8000/api/plate')
+    .then((result)=> {
+      this.array = result.data.response;
+      console.log(this.array);
+    })
+  },
+  methods:{
+    search_plate(){
+      console.log(this.search);
+      this.piattiSelezionati = [];
+      this.array.forEach((item) => {
+        if(item.nome == this.search){
+          this.piattiSelezionati.push(item);
+        }
+      });
+      console.log(this.piattiSelezionati);
+
+    }
+  }
+});
--- resources/views/guest/restaurants/index.blade.php
+++ resources/views/guest/restaurants/index.blade.php
@@ -4,8 +4,8 @@
     <div class="hcg-left">
       <h2> Il piacere del cibo a casa tua</h2>
       <div class="header-searchbar">
-        <input type="text" name="" value="" placeholder="cosa stai cercando?">
-        <button type="button" name="button">Cerca</button>
+        <input v-model='search' type="text" name="" value="" placeholder="cosa stai cercando?">
+        <button @click='search_plate'type="button" name="button">Cerca</button>
       </div>
     </div>
     <div class="hcg-right">
