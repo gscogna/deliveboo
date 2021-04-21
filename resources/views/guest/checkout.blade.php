c1d9b193701162997285f60ef7964248584ae376
Ilaria <ilariamammucari@hotmail.it>
2021-04-21 12:00:52 +0200

    braintree funzionante

--- resources/views/guest/checkout.blade.php
+++ resources/views/guest/checkout.blade.php
@@ -1,5 +1,25 @@
-@extends('layputs.app')
+@extends('layouts.app')
 
 @section('content')
-    
+<div class="container">
+    @if (session('success_message'))
+    <div class="alert alert-success">
+        {{ session('success_message') }}
+    </div>
+    @endif
+
+    @if(count($errors) > 0)
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+    <h1>LA TRANSAZIONE E' AVVENUTA CON SUCCESSO</h1>
+    <button class="btn btn-success">
+        <a href="{{ url('/') }}">Torna alla homepage</a>
+    </button>
+</div>
 @endsection
