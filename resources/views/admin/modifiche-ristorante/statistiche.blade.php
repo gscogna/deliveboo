4962be2e2321beedaaa100c404d84cf4e3221a2e
Ilaria <ilariamammucari@hotmail.it>
2021-04-22 17:50:54 +0200

    modifiche provvisorie

--- resources/views/admin/modifiche-ristorante/statistiche.blade.php
+++ resources/views/admin/modifiche-ristorante/statistiche.blade.php
@@ -2,7 +2,9 @@
 @section('title', 'I tuoi ordini e le tue statistiche')
 
 @section('content')
-<div style="width: 300px">
-    <canvas id="myChart" width="400" height="400"></canvas>
+<div class="container">
+    <div style="width: 300px">
+        <canvas id="myChart" width="400" height="400"></canvas>
+    </div>
 </div>
 @endsection
