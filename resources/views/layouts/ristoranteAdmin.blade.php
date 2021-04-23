6463ce9cde065bbaf63da3d1c7faa91138216916
Gennaro <scognamiglioge@hotmail.it>
2021-04-24 00:16:13 +0200

    prova2

--- resources/views/layouts/ristoranteAdmin.blade.php
+++ resources/views/layouts/ristoranteAdmin.blade.php
@@ -12,18 +12,14 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-      <!-- fontawesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+
     <!--     Fonts and icons     -->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
     <link rel="stylesheet" href="<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">" />
 
     <!-- Styles -->
+    <link href="{{ asset('cssB/now-ui-dashboard.css')}}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="/assets/css/now-ui-dashboard.css" rel="stylesheet">
 
 </head>
 <body>
@@ -35,11 +31,11 @@
         @include('partials.footer')
     </div>
 </body>
-    <script src="../assets/js/core/jquery.min.js" type="text/javascript"></script>
-    <script src="../assets/js/core/popper.min.js" type="text/javascript"></script>
-    <script src="../assets/js/core/bootstrap.min.js" type="text/javascript"></script>
-    <script src="../assets/js/plugins/perfect-scrollbar.jquery.min.js" type="text/javascript"></script>
+    <script src="{{ asset('jsB/core/jquery.min.js')}}" type="text/javascript"></script>
+    <script src="{{ asset('jsB/core/popper.min.js')}}" type="text/javascript"></script>
+    <script src="{{ asset('jsB/core/bootstrap.min.js')}}" type="text/javascript"></script>
+    <script src="{{ asset('jsB/plugins/perfect-scrollbar.jquery.min.js')}}" type="text/javascript"></script>
 
     <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
-    <script src="../assets/js/now-ui-dashboard.js" type="text/javascript"></script>
+    <script src="{{ asset('jsB/now-ui-dashboard.js')}}" type="text/javascript"></script>
 </html>
