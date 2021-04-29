b869aef6f4e0a15f8de6dc765bc4df9f65de26a6
Ilaria <ilariamammucari@hotmail.it>
2021-04-29 09:38:11 +0200

    spazio login register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,7 +14,7 @@
     @endif
 
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8 mb-5">
             <div class="card">
                 <div class="card-header">{{ __('Register') }}</div>
 
